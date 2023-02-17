127814a936a15a3331fbc43a84cbcb461a893814
Fenicsbird <natalya92281979@gmail.com>
2023-02-17 12:46:02 +0200

    complete task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,25 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const ten_dot = /.{10}/g
+    const arr = expr.match(ten_dot);
+
+    const morse_arr = [];
+    arr.forEach(function (element) {
+        element = element.replace(/10/g,'.').replace(/11/g,'-').replace(/00/g,'');
+        morse_arr.push(element);
+    });
+
+    let string = '';
+    morse_arr.forEach(function (element) {
+        if(element === '**********'){
+            string += ' ';
+        } else{
+            string += MORSE_TABLE[element];
+        }
+    });
+
+    return string;
 }
 
 module.exports = {
